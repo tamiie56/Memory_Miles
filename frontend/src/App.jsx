68dcb733936c3502f39f963e2d3e6b53083652dc
tamiie56 <140138497+tamiie56@users.noreply.github.com>
2025-12-05 15:53:50 +0600

    React-router-dom installation & setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home/Home"
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
 
 const App = () => {
   return (
-    <div class='text-red-500 text-4xl'>App</div>
+    <>
+      <BrowserRouter> 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} /> 
+          </Routes>  
+      </BrowserRouter>
+    </>
   )
 }
 
