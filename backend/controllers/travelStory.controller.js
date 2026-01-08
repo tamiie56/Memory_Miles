ae56812299068a218cee23bbddf9949ea79dc386
tamiie56 <140138497+tamiie56@users.noreply.github.com>
2026-01-09 03:07:22 +0600

    delete image api route

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -1,5 +1,8 @@
+import { fileURLToPath } from "url"
 import TravelStory from "../models/travelStory.model.js"
 import { errorHandler } from "../utils/error.js"
+import path from "path"
+import fs from "fs"
 
 export const addTravelStory = async (req, res, next) => {
     const{ title, story, visitedLocation, isFavorite, imageUrl, visitedDate } = req.body
@@ -54,9 +57,45 @@ export const imageUpload = async (req, res, next) => {
             return next (errorHandler(400, "No image uploaded"))
         }
 
-        const imageUrl = 'http://localhost:3000/uploads/${req.file.filename}'
+        const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`
 
         res.status(201).json({imageUrl})
+    } catch(error){
+        next(error)
+    }
+}
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const rootDir = path.join(__dirname, "..")
+
+export const deleteImage = async (req, res, next) => {
+    const {imageUrl} =req.query
+
+    if(!imageUrl){
+        return next(errorHandler(400, "Image URL is required"))
+    }
+
+    try{
+        //extract filename from imageUrl
+        const filename = path.basename(imageUrl)
+
+        //delete the file path
+        const filePath = path.join(rootDir,"uploads", filename)
+
+        //console.log(filePath)
+
+        //check if the file exists
+        if (!fs.existsSync(filePath)){
+            return next(errorHandler(404, "Image not found"))
+        }
+
+        //delete the file
+        await fs.promises.unlink(filePath)
+
+        res.status(200).json({message: "Image deleted successfully"})
+
     } catch(error){
         next(error)
     }
--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { addTravelStory, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
+import { addTravelStory, deleteImage, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 const router = express.Router()
 
 router.post("/image-upload",upload.single("image"), imageUpload)
+
+router.delete("/delete-image", deleteImage)
 
 router.post("/add", verifyToken, addTravelStory)
 
