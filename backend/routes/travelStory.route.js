15aaa6fb3a6443426de9567aadceeeb3828c3683
tamiie56 <140138497+tamiie56@users.noreply.github.com>
2026-01-10 03:37:02 +0600

    update is favorite api route

--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { addTravelStory, deleteImage, deleteTravelStory, editTravelStory, getAllTravelStory, imageUpload } from "../controllers/travelStory.controller.js"
+import { addTravelStory, deleteImage, deleteTravelStory, editTravelStory, getAllTravelStory, imageUpload, updateIsFavorite } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 const router = express.Router()
@@ -16,5 +16,7 @@ router.get("/get-all", verifyToken, getAllTravelStory)
 router.post("/edit-story/:id", verifyToken, editTravelStory)
 
 router.delete("/delete-story/:id", verifyToken, deleteTravelStory)
+
+router.put("/update-is-favorite/:id", verifyToken, updateIsFavorite)
 
 export default router
