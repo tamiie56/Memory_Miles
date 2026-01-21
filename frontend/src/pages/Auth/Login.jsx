297a331c68590126afdd01b2de33433874bedf94
tamiie56 <140138497+tamiie56@users.noreply.github.com>
2026-01-21 22:20:19 +0600

    add react redux toolkit

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -3,14 +3,19 @@ import PasswordInput from "../../components/PasswordInput"
 import { useNavigate } from "react-router-dom"
 import axiosInstance from "../../utils/axiosInstance"
 import { validateEmail } from "../../utils/helper"
+import { useDispatch, useSelector } from "react-redux"
+import { signInStart, signInSuccess } from "../../redux/slice/userSlice"
 
 const Login = () => {
   
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("")
+
+  const { loading } = useSelector((state) => state.user)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -29,12 +34,15 @@ const Login = () => {
 
     // Login API call
     try {
+      dispatch(signInStart())
+
       const response = await axiosInstance.post("/auth/signin", {
         email,
         password,
       })
 
       if (response.data) {
+        dispatch(signInSuccess(response.data))
         navigate("/")
       }
     } catch (error) {
@@ -89,9 +97,13 @@ const Login = () => {
 
              {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
-            <button type="submit" className="btn-primary">
-              LOGIN
-            </button>
+            {loading ? (
+              <span className="animate-pulse">Loading...</span>
+            ) : (
+              <button type="submit" className="btn-primary">
+                LOGIN
+              </button>
+            )}
 
             <p className="text-xs text-slate-500 text-center my-4">Or</p>
 
