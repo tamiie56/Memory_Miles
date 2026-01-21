4012b2f080a2758ac773d8b5d404bb11cff0bf8d
tamiie56 <140138497+tamiie56@users.noreply.github.com>
2026-01-22 01:32:35 +0600

    navbar UI

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -3,3 +3,19 @@ export const validateEmail = (email) => {
 
   return regex.test(email)
 }
+
+
+// Arya Stark blablabla
+export const getInitials = (name) => {
+  if (!name) return ""
+
+  const words = name.split(" ")
+
+  let initials = ""
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0]
+  }
+
+  return initials.toUpperCase()
+}
