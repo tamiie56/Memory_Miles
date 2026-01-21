0f8937cd45876967c84b27acc1b7736a48497f55
tamiie56 <140138497+tamiie56@users.noreply.github.com>
2026-01-22 02:58:49 +0600

    travel story card component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import Navbar from "../../components/Navbar"
 import axiosInstance from "../../utils/axiosInstance"
+import TravelStoryCard from "../../components/TravelStoryCard"
 
 const Home = () => {
   const [allStories, setAllStories] = useState([])
@@ -10,7 +11,7 @@ const Home = () => {
   // Get all travel stories
   const getAllTravelStories = async () => {
     try {
-      const response = await axiosInstance.get("/travel-story/get-all")
+      const response = await axiosInstance.get("/travelstory/get-all")
 
       if (response.data && response.data.stories) {
         setAllStories(response.data.stories)
@@ -19,6 +20,13 @@ const Home = () => {
       console.log("Something went wrong. Please try again.")
     }
   }
+
+  // Handle Edit Story
+  const handleEdit = async (data) => {}
+
+  const handleViewStory = (data) => {}
+
+  const updateIsFavourite = async (data) => {}
 
   useEffect(() => {
     getAllTravelStories()
@@ -32,7 +40,30 @@ const Home = () => {
 
       <div className="container mx-auto py-10">
         <div className="flex gap-7">
-          <div className="flex-1"></div>
+          <div className="flex-1">
+            {allStories.length > 0 ? (
+              <div className="grid grid-cols-2 gap-4">
+                {allStories.map((item) => {
+                  return (
+                    <TravelStoryCard
+                      key={item._id}
+                      imageUrl={item.imageUrl}
+                      title={item.title}
+                      story={item.story}
+                      date={item.visitedDate}
+                      visitedLocation={item.visitedLocation}
+                      isFavourite={item.isFavourite}
+                      onEdit={() => handleEdit(item)}
+                      onClick={() => handleViewStory(item)}
+                      onFavouriteClick={() => updateIsFavourite(item)}
+                    />
+                  )
+                })}
+              </div>
+            ) : (
+              <div>Empty Card Here</div>
+            )}
+          </div>
 
           <div className="w-[320px]"></div>
         </div>
